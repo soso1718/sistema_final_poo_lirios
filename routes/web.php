e4b3fc212788e808cc8b752bfea3d5061ec76a4b
soso1718 <anasophiasantiago679@gmail.com>
2025-10-12 19:15:19 -0300

    Login feito (com breeze), controle de acesso em todas páginas
    Tailwind também foi instalado, mas não foi usado ainda

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,35 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
-use App\Http\Controllers\StockController;
 use App\Http\Controllers\CatalogController;
 use App\Http\Controllers\ProfessionalsController;
+use App\Http\Controllers\StockController;
+use App\Http\Controllers\SupplierController;
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('patients', PatientController::class);
-Route::resource('stocks', StockController::class);
-Route::resource('catalogs', CatalogController::class);
-Route::resource('professionals', ProfessionalsController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::middleware('auth')->group(function () {
+    Route::resource('patients', PatientController::class);
+    Route::resource('catalogs', CatalogController::class);
+    Route::resource('professionals', ProfessionalsController::class);
+    Route::resource('stocks', StockController::class);
+    Route::resource('suppliers', SupplierController::class);
+});
+
