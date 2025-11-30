4e7767989d233b7bc17cfc8782274cbda390e6ac
soso1718 <anasophiasantiago679@gmail.com>
2025-11-29 21:51:43 -0300

    Criação do SHOW em todos os CRUDSs
    Adicionado SHOW aos controllers e criado uma view show para cada um dos cruds

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,19 @@ use App\Http\Controllers\CatalogController;
 use App\Http\Controllers\ProfessionalsController;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\SchedulingController;
+use App\Http\Controllers\HomeController;
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])
+->middleware(['auth'])
+->name('home');
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -33,5 +37,6 @@ Route::middleware('auth')->group(function () {
     Route::resource('professionals', ProfessionalsController::class);
     Route::resource('stocks', StockController::class);
     Route::resource('suppliers', SupplierController::class);
+    Route::resource('schedulings', SchedulingController::class);
 });
 
